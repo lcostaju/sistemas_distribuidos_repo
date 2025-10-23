ea9a30fd9181dc48dfec4338cb8fbbe7f505663d
lcostaju <luciocj_eng@outlook.com>
2025-10-22 22:11:36 -0300

    Refactor code structure and remove redundant code blocks for improved readability and maintainability.

---
+++ exemplo_protobuf/src/main/java/proto/exemplo_protobuf/ExemploProtobufApplication.java
@@ -0,0 +1,13 @@
+package proto.exemplo_protobuf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploProtobufApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploProtobufApplication.class, args);
+	}
+
+}
---
+++ exemplo_protobuf/src/test/java/proto/exemplo_protobuf/ExemploProtobufApplicationTests.java
@@ -0,0 +1,13 @@
+package proto.exemplo_protobuf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploProtobufApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
